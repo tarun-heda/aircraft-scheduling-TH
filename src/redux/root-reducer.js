68eddf70a2ff907be347a99a5a8cae260e48a623
Tarun <tarun.heda@sevenlakes.com>
2021-08-04 15:20:36 -0700

    removing commented out code

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,7 +1,6 @@
 import { combineReducers } from 'redux';
 
 import FlightReducer from './reducers/FlightReducer';
-// import TweetListReducer from './reducers/TweetListReducer';
 
 export default combineReducers({
     Flight: FlightReducer
--- src/redux/store.js
+++ src/redux/store.js
@@ -3,6 +3,5 @@ import { createStore } from 'redux';
 import rootReducer from './root-reducer'
 
 const store = createStore(rootReducer);
-// const store = createStore();
 
 export default store;
